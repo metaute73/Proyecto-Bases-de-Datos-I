3c0bf487a622f5786e5d10bc8dcf308141392928
metaute73 <79713985+metaute73@users.noreply.github.com>
2022-11-25 17:00:17 -0500

    database update

--- Ejemplo_trabajo_final_BD1/config/conexion.php
+++ Ejemplo_trabajo_final_BD1/config/conexion.php
@@ -6,6 +6,6 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "ejemplo";
+$db = "sia";
 
 $conn = new mysqli($host, $user, $pass, $db) or die("Error al conectar a la DB " . mysqli_error($link));
